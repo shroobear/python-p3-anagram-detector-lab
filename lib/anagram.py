875d675bf51d058f5190d972dc0422d1ad226c8f
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-07-21 08:56:01 -0400

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        match_list = []
+        sorted_self_word = sorted(self.word)
+        for word in word_list:
+            if sorted_self_word == sorted(word):
+                match_list.append(word)
+        print(match_list)
+        return match_list
